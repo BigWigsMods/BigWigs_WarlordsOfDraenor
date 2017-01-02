c84e2c3b5440c46899a8cfc642272d8ee8dd1501
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2017-01-02 14:33:14 -0300

    Update ptBR translations (#3)

--- HellfireCitadel/Locales/ptBR.lua
+++ HellfireCitadel/Locales/ptBR.lua
@@ -1,118 +1,122 @@
-local L = BigWigs:NewBossLocale("Hellfire Assault", "ptBR")
-if not L then return end
+local L = BigWigs:NewBossLocale("Archimonde", "ptBR")
 if L then
--- L.left = "Left: %s"
--- L.middle = "Middle: %s"
--- L.right = "Right: %s"
+	L.torment_removed = "Tormento removido (%d/%d)"
+	L.chaos_bar = "%s -> %s"
+	L.chaos_from = "%s de %s"
+	L.chaos_to = "%s para %s"
+	L.infernal_count = "%s (%d/%d)"
 
-end
+	L.custom_off_torment_marker = "Marcador de Tormento Agrilhoado"
+	L.custom_off_torment_marker_desc = "Marca os alvos de Tormento Agrilhoado com {rt1}{rt2}{rt3}, requer assistente ou líder."
 
-L = BigWigs:NewBossLocale("Kilrogg Deadeye", "ptBR")
-if L then
--- L.add_warnings = "Add Spawn Warnings"
+	L.markofthelegion_self = "Marca da Legião em você"
+	L.markofthelegion_self_desc = "Contador especial para quando Marca da Legião está em você."
+	L.markofthelegion_self_bar = "Você explodiu!"
 
-end
+	L.custom_off_legion_marker = "Marcador de Marca da Legião"
+	L.custom_off_legion_marker_desc = "Marca os alvos de Marca da Legião com {rt1}{rt2}{rt3}{rt4}, requer assistente ou líder."
 
-L = BigWigs:NewBossLocale("Gorefiend", "ptBR")
-if L then
--- L.fate_root_you = "Shared Fate - You are rooted!"
--- L.fate_you = "Shared Fate on YOU! - Root on %s"
+	L.custom_off_infernal_marker = "Marcador de Infernal"
+	L.custom_off_infernal_marker_desc = "Marca os Infernais invocados por Chuva do Caos com {rt1}{rt2}{rt3}{rt4}{rt5}, requer assistente ou líder."
 
-end
-
-L = BigWigs:NewBossLocale("Shadow-Lord Iskar", "ptBR")
-if L then
--- L.bindings_removed = "Bindings removed (%d/3)"
--- L.custom_off_binding_marker = "Dark Bindings marker"
---[==[ L.custom_off_binding_marker_desc = [=[Mark the Dark Bindings targets with {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}, requires promoted or leader.
-|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r]=] ]==]
--- L.custom_off_wind_marker = "Phantasmal Winds marker"
---[==[ L.custom_off_wind_marker_desc = [=[Marks Phantasmal Winds targets with {rt1}{rt2}{rt3}{rt4}{rt5}, requires promoted or leader.
-|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r]=] ]==]
-
-end
-
-L = BigWigs:NewBossLocale("Socrethar the Eternal", "ptBR")
-if L then
--- L.dominator_desc = "Warnings for when the Sargerei Dominator spawns."
--- L.portals = "Portals Move"
--- L.portals_desc = "Timer for when the portals in phase 2 change positions."
--- L.portals_msg = "The portals have moved!"
-
+	L.custom_off_chaos_helper = "Assistente de Caos Desencadeado"
+	L.custom_off_chaos_helper_desc = "Na dificuldade mítica apenas. Esta ferramenta vai mostrar seu número na mecânica de Caos Desencadeado, mostrando para você uma mensagem e anunciando no chat. Dependendo da tática que você usar esta funcionalidade pode ou não ser útil."
+	L.chaos_helper_message = "Sua posição no Caos: %d"
 end
 
 L = BigWigs:NewBossLocale("Fel Lord Zakuun", "ptBR")
 if L then
--- L.custom_off_seed_marker = "Seed of Destruction marker"
--- L.custom_off_seed_marker_desc = "Mark the Seed of Destruction targets with {rt1}{rt2}{rt3}{rt4}{rt5}, requires promoted or leader."
--- L.seed = "Seed"
--- L.tank_proximity = "Tank Proximity"
--- L.tank_proximity_desc = "Open a 5 yard proximity showing the other tanks to help you deal with the Heavy Handed & Heavily Armed abilities."
+	L.seed = "Semente"
 
+	L.custom_off_seed_marker = "Marcador de Semente da Destruição"
+	L.custom_off_seed_marker_desc = "Marca os alvos de Semente da Destruição com {rt1}{rt2}{rt3}{rt4}{rt5}, requer assistente ou líder."
+
+	L.tank_proximity = "Proximidade do Tanque"
+	L.tank_proximity_desc = "Abre uma janela de proximidade de 5 metros mostrando os outros tanques para ajudar a lidar com as habilidades Mão Pesada & Fortemente Armado"
 end
 
-L = BigWigs:NewBossLocale("Tyrant Velhari", "ptBR")
+L = BigWigs:NewBossLocale("Gorefiend", "ptBR")
 if L then
--- L.font_removed_soon = "Your Font expires soon!"
+	L.fate_root_you = "Destino Compartilhado - Você está enraizado!"
+	L.fate_you = "Destino Compartilhado em VOCÊ! - Enraizar em %s"
+end
 
+L = BigWigs:NewBossLocale("Hellfire Assault", "ptBR")
+if L then
+	L.left = "Esquerda: %s"
+	L.middle = "Meio: %s"
+	L.right = "Direita: %s"
+end
+
+L = BigWigs:NewBossLocale("Kilrogg Deadeye", "ptBR")
+if L then
+	L.add_warnings = "Alerta de surgimento de adds"
 end
 
 L = BigWigs:NewBossLocale("Mannoroth", "ptBR")
 if L then
--- L["182212"] = "Infernal portal closed!"
--- L["185147"] = "Doom Lord portal closed!"
--- L["185175"] = "Imp portal closed!"
--- L.custom_off_doom_marker = "Mark of Doom marker"
--- L.custom_off_doom_marker_desc = "On Mythic difficulty, mark the targets of Mark of Doom with {rt1}{rt2}{rt3}, requires promoted or leader."
--- L.custom_off_gaze_marker = "Mannoroth's Gaze marker"
--- L.custom_off_gaze_marker_desc = "Mark the targets of Mannoroth's Gaze with {rt1}{rt2}{rt3}, requires promoted or leader."
--- L.custom_off_wrath_marker = "Wrath of Gul'dan marker"
--- L.custom_off_wrath_marker_desc = "Mark the targets of Wrath of Gul'dan with {rt8}{rt7}{rt6}{rt5}{rt4}, requires promoted or leader."
-L.felseeker_message = "%s (%d) %dm" -- Needs review
--- L.gaze = "Gaze (%d)"
+	L["185147"] = "Portal de Senhor da Perdição fechado!"
+	L["185175"] = "Portal de Diabrete Vil fechado!"
+	L["182212"] = "Portal de Infernal Medonho fechado!"
 
+	L.gaze = "Olhar (%d)"
+	L.felseeker_message = "%s (%d) %dm"
+
+	L.custom_off_gaze_marker = "Marcador de Olhar de Mannoroth"
+	L.custom_off_gaze_marker_desc = "Marca os alvos de Olhar de Mannoroth com {rt1}{rt2}{rt3}, requer assistente ou líder."
+
+	L.custom_off_doom_marker = "Marcador de Marca da Ruína"
+	L.custom_off_doom_marker_desc = "Na dificuldade mítica, marca os alvos de Marca da Ruína com {rt1}{rt2}{rt3}, requer assistente ou líder."
+
+	L.custom_off_wrath_marker = "Marcador de Ira de Gul'dan"
+	L.custom_off_wrath_marker_desc = "Marca os alvos de Ira de Gul'dan com {rt8}{rt7}{rt6}{rt5}{rt4}, requer assistente ou líder."
 end
 
-L = BigWigs:NewBossLocale("Archimonde", "ptBR")
+L = BigWigs:NewBossLocale("Shadow-Lord Iskar", "ptBR")
 if L then
-L.chaos_bar = "%s -> %s" -- Needs review
--- L.chaos_from = "%s from %s"
--- L.chaos_helper_message = "Your Chaos position: %d"
--- L.chaos_to = "%s to %s"
--- L.custom_off_chaos_helper = "Wrought Chaos helper"
--- L.custom_off_chaos_helper_desc = "For Mythic difficulty only. This feature will tell you what chaos number you are, showing you a normal message and printing to say chat. Depending on what tactic you use, this feature may or may not be useful."
--- L.custom_off_infernal_marker = "Infernal marker"
--- L.custom_off_infernal_marker_desc = "Mark the Infernals spawned by Rain of Chaos with {rt1}{rt2}{rt3}{rt4}{rt5}, requires promoted or leader."
--- L.custom_off_legion_marker = "Mark of the Legion marker"
--- L.custom_off_legion_marker_desc = "Mark the Mark of the Legion targets with {rt1}{rt2}{rt3}{rt4}, requires promoted or leader."
--- L.custom_off_torment_marker = "Shackled Torment marker"
--- L.custom_off_torment_marker_desc = "Mark the Shackled Torment targets with {rt1}{rt2}{rt3}, requires promoted or leader."
--- L.infernal_count = "%s (%d/%d)"
--- L.markofthelegion_self = "Mark of the Legion on you"
--- L.markofthelegion_self_bar = "You explode!"
--- L.markofthelegion_self_desc = "Special countdown when Mark of the Legion is on you."
--- L.torment_removed = "Torment removed (%d/%d)"
+	L.custom_off_wind_marker = "Marcador de Ventos Fantasmagóricos"
+	L.custom_off_wind_marker_desc = "Marca os alvos de Ventos Fantasmagóricos com {rt1}{rt2}{rt3}{rt4}{rt5}, requer assistente ou líder.\n|cFFFF0000Apenas 1 pessoa na raide deve ter esta opção ativada para evitar conflitos de marcação.|r]"
 
+	L.bindings_removed = "Grilhões removidos (%d/3)"
+	L.custom_off_binding_marker = "Marcador de Grilhões Nefastos"
+	L.custom_off_binding_marker_desc = "Marca os alvos de Grilhões Nefastos com {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}, requer assistente ou líder.\n|cFFFF0000Apenas 1 pessoa na raide deve ter esta opção ativada para evitar conflitos de marcação.|r]"
+end
+
+L = BigWigs:NewBossLocale("Socrethar the Eternal", "ptBR")
+if L then
+	L.dominator_desc = "Alertas para quando o Dominador Sangerei chega."
+
+	L.portals = "Movimento dos portais"
+	L.portals_desc = "Contador para quando os portais mudam de posição na fase 2."
+	L.portals_msg = "Os portais se moveram!"
 end
 
 L = BigWigs:NewBossLocale("Hellfire Citadel Trash", "ptBR")
 if L then
-L.anetheron = "Anetheron" -- Needs review
-L.azgalor = "Azgalor" -- Needs review
-L.bloodthirster = "Sanguissedento Salivante" -- Needs review
-L.burster = "Rebentador Sombrio" -- Needs review
-L.daggorath = "Dag'gorath" -- Needs review
-L.darkcaster = "Taumaturgo Sangrando" -- Needs review
-L.eloah = "Vinculador Eloah" -- Needs review
-L.enkindler = "Incandescente Fogosa" -- Needs review
-L.faithbreaker = "Quebra-fé Eredar" -- Needs review
-L.graggra = "Graggra" -- Needs review
-L.kazrogal = "Kaz'rogal" -- Needs review
-L.kuroh = "Adjunta Kuroh" -- Needs review
-L.orb = "Orbe da Destruição" -- Needs review
-L.peacekeeper = "Constructo Pacificador" -- Needs review
-L.talonpriest = "Sacerdote da Garra Corrompido" -- Needs review
-L.weaponlord = "Mestre das Armas Mehlkhior" -- Needs review
-
+	L.orb = "Orbe da Destruição"
+	L.enkindler = "Incandescente Fogosa"
+	L.graggra = "Graggra"
+	L.darkcaster = "Taumaturgo Sangrando"
+	L.bloodthirster = "Sanguissedento Salivante"
+	L.talonpriest = "Sacerdote da Garra Corrompido"
+	L.peacekeeper = "Constructo Pacificador"
+	L.eloah = "Vinculador Eloah"
+	L.faithbreaker = "Quebra-fé Eredar"
+	L.kuroh = "Adjunta Kuroh"
+	L.daggorath = "Dag'gorath"
+	L.burster = "Rebentador Sombrio"
+	L.weaponlord = "Mestre das Armas Mehlkhior"
+	L.azgalor = "Azgalor"
+	L.kazrogal = "Kaz'rogal"
+	L.anetheron = "Anetheron"
 end
 
+L = BigWigs:NewBossLocale("Tyrant Velhari", "ptBR")
+if L then
+	L.font_removed_soon = "Sua Fonte termina logo!"
+end
+
+L = BigWigs:NewBossLocale("Xhul'horac", "ptBR")
+if L then
+	L.killed = "%s morto!"
+end
